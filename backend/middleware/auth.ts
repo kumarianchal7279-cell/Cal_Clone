f024adb3438cd75c31ce7627dbfdc28fa3942564
Anchal Kumari <kumarianchal7279@gmail.com>
2026-03-29 14:27:19 +0530

    Pushing changes

--- backend/middleware/auth.ts
+++ backend/middleware/auth.ts
@@ -4,10 +4,6 @@ import db from '../db';
 
 export interface AuthRequest extends Request {
   userId?: string;
-  body?: any;
-  params?: any;
-  query?: any;
-  headers?: any;
 }
 
 export const authMiddleware = async (req: AuthRequest, res: Response, next: NextFunction) => {
